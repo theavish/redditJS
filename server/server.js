8601722eb9896f49e144a1db1f57102f37a29e1b
Avi <avi.samloff@gmail.com>
2016-04-14 15:42:11 -0700

    fixes serving from client

--- server/server.js
+++ server/server.js
@@ -4,12 +4,13 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 8080;
+const clientDir = path.join(__dirname, '..', 'client');
 
 const apiRouter = require('./routes/router.js')
 
 app.use(morgan('dev'));
 
+app.use('/', express.static(clientDir));
 app.use('/api', apiRouter);
-app.use('/', express.static(path.join(__dirname, 'client')));
 
 app.listen(port, () => { console.log('listening on', port) });
