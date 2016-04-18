45e00bd933d59d8e0292f5896460d0821c524d80
Avi <avi.samloff@gmail.com>
2016-04-18 13:22:11 -0700

    reorganizes file system

--- server/server.js
+++ server/server.js
@@ -4,13 +4,20 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 8080;
-const clientDir = path.join(__dirname, '..', 'client');
+const devDir = path.join(__dirname, '..', 'client', 'dev');
+const distDir = path.join(__dirname, '..', 'client', 'dist');
 
 const apiRouter = require('./routes/router.js')
 
-app.use(morgan('dev'));
 
-app.use('/', express.static(clientDir));
+
+if (node.env.devFlag) {
+  app.use('/', express.static(devDir));
+  app.use(morgan('dev'));
+} else {
+  app.use('/', express.static(distDir));
+}
+
 app.use('/api', apiRouter);
 
 app.listen(port, () => { console.log('listening on', port) });
